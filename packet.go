6be16020d14c1359e27e20a697afffd2553eb0e6
sauerbraten <willing.alexander@googlemail.com>
2017-05-29 20:08:59 +0200

    use bytes.Buffer; support writing to a packet

--- packet.go
+++ packet.go
@@ -1,78 +1,102 @@
 package cubecode
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
+var ErrBufferTooShort = errors.New("cubecode: buffer too short")
+
 // Packet represents a Sauerbraten UDP packet.
 type Packet struct {
-	buf      []byte
-	posInBuf int
+	buf *bytes.Buffer
 }
 
 // NewPacket returns a Packet using buf as the underlying buffer.
 func NewPacket(buf []byte) *Packet {
 	return &Packet{
-		buf:      buf,
-		posInBuf: 0,
+		buf: bytes.NewBuffer(buf),
 	}
 }
 
-// Len returns the length of the packet in bytes.
+// Len returns the number of unread bytes left in the packet.
 func (p *Packet) Len() int {
-	return len(p.buf)
+	return p.buf.Len()
 }
 
 // HasRemaining returns true if there are bytes remaining to be read in the packet.
 func (p *Packet) HasRemaining() bool {
-	return p.posInBuf < len(p.buf)
+	return p.Len() > 0
 }
 
 // SubPacketFromRemaining returns a packet from the bytes remaining in p's buffer.
 func (p *Packet) SubPacketFromRemaining() (*Packet, error) {
-	return p.SubPacket(p.posInBuf, len(p.buf))
+	return p.SubPacket(p.Len())
 }
 
 // SubPacket returns a part of the packet as new packet. It does not copy the underlying slice!
-func (p *Packet) SubPacket(start, end int) (q *Packet, err error) {
-	if start < 0 || start > len(p.buf)-1 {
-		err = errors.New("cubecode: invalid start index for packet of length " + strconv.Itoa(len(p.buf)))
-		return
+func (p *Packet) SubPacket(n int) (q *Packet, err error) {
+	if n > p.Len() {
+		return nil, fmt.Errorf("cubecode: sub-packet of length %v requested, but there are only %v bytes left", n, p.Len())
 	}
 
-	if end < 1 || end > len(p.buf) {
-		err = errors.New("cubecode: invalid end index for packet of length " + strconv.Itoa(len(p.buf)))
-		return
-	}
+	return NewPacket(p.buf.Next(n)), nil
+}
 
-	q = NewPacket(p.buf[start:end])
-	return
+func (p *Packet) WriteByte(b byte) error {
+	err := p.buf.WriteByte(b)
+	if err != nil {
+		err = fmt.Errorf("cubecode: %s", err)
+	}
+	return err
+}
+
+// WriteInt writes an int32 to the packet buffer. It grows the packet as needed.
+func (p *Packet) WriteInt(value int32) {
+	if value < 128 && value > -127 {
+		p.buf.WriteByte(byte(value))
+	} else if value < 0x8000 && value >= -0x8000 { // TODO: check this
+		p.buf.WriteByte(0x80)
+		p.buf.WriteByte(byte(value))
+		p.buf.WriteByte(byte(value >> 8))
+	} else {
+		p.buf.WriteByte(0x81)
+		p.buf.WriteByte(byte(value))
+		p.buf.WriteByte(byte(value >> 8))
+		p.buf.WriteByte(byte(value >> 16))
+		p.buf.WriteByte(byte(value >> 24))
+	}
+}
+
+func (p *Packet) WriteString(s string) {
+	for _, c := range s {
+		p.WriteInt(uni2Cube[c])
+	}
+	p.WriteByte(0x00)
 }
 
 // ReadByte returns the next byte in the packet.
 func (p *Packet) ReadByte() (byte, error) {
-	if p.posInBuf < len(p.buf) {
-		p.posInBuf++
-		return p.buf[p.posInBuf-1], nil
+	b, err := p.buf.ReadByte()
+	if err != nil {
+		err = fmt.Errorf("cubecode: %s", err)
 	}
-
-	return 0, errors.New("cubecode: buf overread at position " + strconv.Itoa(p.posInBuf))
+	return b, err
 }
 
 // ReadInt returns the integer value encoded in the next bytes of the packet.
 func (p *Packet) ReadInt() (int, error) {
-	// n is the size of the buffer
-	n := len(p.buf)
+	n := p.Len()
 
 	if n < 1 {
-		return -1, errors.New("cubecode: buf too short")
+		return -1, ErrBufferTooShort
 	}
 
 	var (
-		value int
+		value int32
 		err   error
 		b     byte
 	)
@@ -86,13 +110,13 @@ func (p *Packet) ReadInt() (int, error) {
 	default:
 		// most often, th value is only one byte
 		// convert to int and keep the sign of the 8-bit representation
-		value = int(int8(b))
+		value = int32(int8(b))
 
 	case 0x80:
 		// value is contained in the next two bytes
 		value, err = p.readInt(2)
 		// make sure to keep the sign of the 16-bit representation
-		value = int(int16(value))
+		value = int32(int16(value))
 
 	case 0x81:
 		// value is contained in the next four bytes
@@ -100,13 +124,13 @@ func (p *Packet) ReadInt() (int, error) {
 
 	}
 
-	return value, err
+	return int(value), err
 }
 
-// readInt reads n bytes from the packet and decodes them into an int value.
-func (p *Packet) readInt(n int) (int, error) {
+// readInt reads n bufNewbytes from the packet and decodes them into an int value.
+func (p *Packet) readInt(n int) (int32, error) {
 	var (
-		value int
+		value int32
 		err   error
 		b     byte
 	)
@@ -117,7 +141,7 @@ func (p *Packet) readInt(n int) (int, error) {
 			return -1, err
 		}
 
-		value += int(b) << (8 * uint(i))
+		value += int32(b) << (8 * uint(i))
 	}
 
 	return value, nil
